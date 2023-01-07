4d7271f37909e5408f28eb867eb9c472711050e3
宋海龙 <1298486187@qq.com>
2023-01-07 21:45:08 +0800

    ESP32-C3版本

---
+++ 2.Software/AUTO_Watering_Cli/src/FeaturesFun.cpp
@@ -0,0 +1,79 @@
+#include "FeaturesFun.h"
+#include "main.h"
+
+uint32_t BeginWateringTime; //开始浇水时间记录单位s
+uint32_t lastGetADCTime = 0; //上次ADC读取时间 ms
+
+//计算湿度  0 ~ 100
+float CalculateHumidity(int ADC)
+{
+    float a =  -0.04175;
+    float b = 170.98121;
+    return (float)ADC * a + b;
+}
+
+//浇水函数
+void Watering()
+{
+    if(BeginWatering == true) //通过APP按键强制开始浇水
+    {
+        OpenPump;  //打开水泵
+        BeginWateringTime = millis()/1000; //记录打开水泵时间
+        Is_Watering = true; //正在浇水
+        BeginWatering = false;
+        heartbeat();//更新app界面显示
+    }
+
+    if(Humidity == 0)
+        return;
+
+    if(Humidity < BeginHumidity && Is_Watering == false)  //湿度小于临界值 且 未在浇水
+    {
+        if(NowTime.GetNowTime() == false)
+            return ;
+        //离上次浇水已经过去2天
+        if((NowTime.My_yday + NowTime.My_year * 365) - (LastWaterTime.My_yday + LastWaterTime.My_year * 365)  > 2)
+        {
+            OpenPump;  //打开水泵
+            BeginWateringTime = millis()/1000; //记录打开水泵时间
+            Is_Watering = true; //正在浇水 
+            heartbeat();//更新app界面显示
+        }
+             
+    }
+    if(Is_Watering == true) //如果正在浇水
+    {
+        if((millis()/1000 - BeginWateringTime > WateringTime) || StopWatering == true)
+        {
+            ClosePump; //关闭水泵
+            LastWaterTime2 = LastWaterTime;
+            LastWaterTime.GetNowTime(); //更新浇水时间
+            StopWatering = false;
+            Is_Watering = false; //未在浇水
+            Is_WateringSuccess = true; //自动浇水成功
+            heartbeat();//更新app界面显示
+        }
+    }
+}
+
+/**
+ *调用卡尔曼滤波器 实践
+ */
+KFP KFP_Temp = {0.21, 0.22, -1.11f, 0.05, 0.01, 0.543};
+// 获取湿度
+void GetHumidity()
+{
+    if(millis() - lastGetADCTime < 50)
+        return;
+    uint16_t ADC_RAW = analogRead(ADC_CH);
+//    if(ADC_RAW == 0 || ADC_RAW == 4095)
+//        return;
+    uint16_t ADC;
+    //卡尔曼滤波器
+    ADC = kalmanFilter(&KFP_Temp, (float) ADC_RAW);
+    Humidity = CalculateHumidity(ADC); //计算湿度
+    if (Humidity > 100)
+        Humidity = 100;
+
+    lastGetADCTime = millis();
+}
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# Auto-Watering
-自动浇花系统
+# Auto-Watering 自动浇水系统
+使用ESP32-C3制作的可远程监控和控制的自动浇水系统。接入的平台为点灯科技。
+
+开发环境为 CLion + Platform IO。
+
+------
+
+# 常见问题：
+
+1. 继电器模块无法控制，不管输出高电平还是低电平，继电器都只有一个状态。给继电器供3.3V电源。
+2. 湿度采集不准确，需按照视频重新计算参数。
+3. 浇水后会有短暂的湿度采集出错，可能是供电问题，视频里所有模块都通过一个电源供电，可将水泵和其他模块分开供电。
+
+---
+
+## 一：项目文件介绍
+
+### 1. **Hardware**
+
+Hardware文件夹里是为合宙ESP32-C3开发板的资料。
+
+### 2. **Software**
+
+Software文件夹为程序源码。
+
+### 3. AppInterface
+
+AppInterface文件夹中提供了APP界面配置文件。
+
+### 4. Docs
+
+相关的参考文件，包括芯片的Datasheet等。
+
+## 二：硬件说明
+
+### 关于主控：
+
+主控用的是合宙的ESP32-C3开发板，淘宝链接为 [合宙ESP32C3开发板](https://item.taobao.com/item.htm?spm=a1z09.2.0.0.3ed92e8dKTe2kF&id=666579064570&_u=71vdumg4341e) ，选经典款。也可以使用其他ESP32-C3的开发板，但是烧入程序时需注释掉 platformio.ini 文件里的 `board_build.flash_mode = dio` 。
+
+### 关于湿度检测模块：
+
+使用电容式土壤湿度检测模块。
+
+使用时最好将上半部分电路密封，防止水溅入，可使用胶枪密封。
+
+![img](https://s2.loli.net/2023/01/07/btc4XxvM6NiLsTz.jpg)
+
+## 三：程序说明
+
+将main.cpp中的如下代码修改为自己的，设备密匙见下文。
+
+<img src="https://s2.loli.net/2023/01/07/87GuI9lsEBdhXkP.png" alt="image-20230107212958489" style="zoom:75%;" />
+
+## 四：APP配置
+
+### 1. 下载并安装app
+
+app下载链接：https://diandeng.tech/dev ，选择安卓或者苹果版本下载。
+
+<img src="https://s2.loli.net/2023/01/07/BzD7MwijQgmUAr9.png" alt="image-20230107214244651" style="zoom: 33%;" />
+
+安装好后注册一个账号并登录。
+
+### 2. 在app中添加设备，获取Secret Key
+
+进入App，点击右上角的“+”号，然后选择添加设备，点击选择Arduino -> WiFi接入-> 选择要接入的服务商 -> 复制申请到的Secret Key
+
+<img src="https://s2.loli.net/2023/01/07/P8p61NKmslbS9vU.png" style="zoom:80%;" />
+
+### 3. 配置界面
+
+在设备列表页，点击刚才新建的设备图标，进入设备控制面板。首次进入设备控制面板，会弹出向导页，此处随便选。
+
+再点击右上角的 **... > 界面配置**，粘贴[界面配置_v2.0.txt](3.AppInterface/界面配置_v2.0.txt)中的配置代码即可。
